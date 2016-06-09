79604515870efb8b11c4033a74147e5c88c74176
MGRiv <mgrivera501@gmail.com>
2016-06-09 16:48:05 -0400

    home.js bug fix

--- static/js/home.js
+++ static/js/home.js
@@ -3,7 +3,8 @@ function getThumbs() {
     $.get('/getsamples', function(e) {
 	console.log('getsamples');
 	var paths = JSON.parse(e);
-	for(i in paths){
+	var i = 0;
+	for(i in paths)(function(i){
 	    var img = new Image();
 	    img.src = "../static/" + paths[i][1];
 	    console.log(paths[i][0]);
@@ -16,7 +17,7 @@ function getThumbs() {
 		while(count < temp.length - 2){
 		    path += temp[count];
 		};
-		path += "/gallery/" + paths[i][0]
+		path += "/gallery/" + paths[i][0];
 		window.location.replace(path);
 	    };
 	    console.log(img.src);
@@ -24,7 +25,7 @@ function getThumbs() {
 	    document.getElementById("d"+i.toString()).appendChild(img);
 	    $("#d" + i.toString()).append("<div class='imgTitle'>" + paths[i][0] +
 					  "</div>");
-	};
+	})(i);
     });
 };
 

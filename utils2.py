d2926e0ec605ac6232d6ea5a7a1f40bfa6629a10
MGRiv <mgrivera501@gmail.com>
2016-06-12 17:37:21 -0400

    bug fix: rename thluffy

--- utils2.py
+++ utils2.py
@@ -180,7 +180,7 @@ def get_sample_images():  #gets one image from each gallery
         try:
             dict["path"] = sql_out[0][0]
         except IndexError:
-            dict["path"] = os.path.join( os.path.dirname(__file__), "images/thluffy-big.png")
+            dict["path"] = os.path.join( os.path.dirname(__file__), "images")
         out.append(dict)
     return out
     
